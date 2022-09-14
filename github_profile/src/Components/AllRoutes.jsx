7af1db8ed09fbef9a0c65ddaec77ef22bf5be3b2
Abhijeet Dharme <abhijeetpdharme@gmail.com>
2022-09-14 10:19:57 +0530

    add

--- github_profile/src/Components/AllRoutes.jsx
+++ github_profile/src/Components/AllRoutes.jsx
@@ -3,9 +3,9 @@ import { Route, Routes } from 'react-router-dom'
 import { UserProfile } from '../Pages/UserProfile'
 
 export const AllRoutes = () => {
-  return (
-    <Routes>
-    <Route path='/' element={<UserProfile/>} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path='/' element={<UserProfile />} />
+        </Routes>
+    )
 }
